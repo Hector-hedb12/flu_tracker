2d010d4e4d4b0663d74558e4fa62fb5d8e7778a7
Hector Dominguez <hedb_12@hotmail.com>
2018-02-20 15:57:59 -0500

    initial world app.

--- config/settings/base.py
+++ config/settings/base.py
@@ -55,6 +55,7 @@ THIRD_PARTY_APPS = [
 # Apps specific for this project go here.
 LOCAL_APPS = [
     'flu_tracker.users.apps.UsersConfig',
+    'flu_tracker.world.apps.WorldConfig',
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
