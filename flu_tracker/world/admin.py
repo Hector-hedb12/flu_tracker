8bbcdacdc0de7cf7b5c7b0697b58ba62e91a80bd
Hector Dominguez <hedb_12@hotmail.com>
2018-02-20 22:27:25 -0500

    include admin for `ZipCode` model

--- flu_tracker/world/admin.py
+++ flu_tracker/world/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.gis import admin
-from .models import WorldBorder
+from .models import WorldBorder, ZipCode
 
 admin.site.register(WorldBorder, admin.OSMGeoAdmin)
+admin.site.register(ZipCode, admin.OSMGeoAdmin)
