a7093a87f659ddbc4fb7fcb73f38444ce1acbfbe
Hector Dominguez <hedb_12@hotmail.com>
2018-02-22 14:30:22 -0500

    remove transforming geometry error.

--- flu_tracker/world/widgets.py
+++ flu_tracker/world/widgets.py
@@ -4,6 +4,7 @@ from django.contrib.gis.forms import OSMWidget
 class ReadonlyOSMWidget(OSMWidget):
     template_name = 'widgets/openlayers-readonly-osm.html'
     disabled = True
+    map_srid = 4326  # before: 3857
 
     def __init__(self, attrs=None):
         super().__init__()
