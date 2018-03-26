050fc456a120f60b19041de46ccb458619167701
Hector Dominguez <hedb_12@hotmail.com>
2018-03-25 22:39:27 -0400

    fix some issues in forms

--- flu_tracker/tracker/forms.py
+++ flu_tracker/tracker/forms.py
@@ -2,12 +2,20 @@ from django.contrib.gis import forms
 
 from .widgets import ReadonlyOSMWidget
 
+DEFAULT_LAT = 45.4215
+DEFAULT_LON = -75.6972
+DEFAULT_ZOOM = 8
+
 
 class PolygonForm(forms.Form):
     poly = forms.PolygonField(
         widget=forms.OSMWidget(attrs={
             'map_width': 800,
-            'map_height': 500
+            'map_height': 500,
+            'map_srid': 4326,
+            'default_lat': DEFAULT_LAT,
+            'default_lon': DEFAULT_LON,
+            'default_zoom': DEFAULT_ZOOM,
         })
     )
 
@@ -16,6 +24,7 @@ class TrackerForm(forms.Form):
     collection = forms.GeometryCollectionField(
         widget=ReadonlyOSMWidget(attrs={
             'map_width': 800,
-            'map_height': 500
+            'map_height': 500,
+            'map_srid': 4326,
         })
     )
