f37953a2a4c77a04e8da643dea5e882ef2b8c916
Hector Dominguez <hedb_12@hotmail.com>
2018-03-31 00:16:12 -0400

    improve the admin site for Tweet instances

--- flu_tracker/tracker/admin.py
+++ flu_tracker/tracker/admin.py
@@ -1,4 +1,26 @@
 from django.contrib.gis import admin
+from django.utils.safestring import mark_safe
+
 from .models import Tweet
 
-admin.site.register(Tweet, admin.OSMGeoAdmin)
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('ref', 'created', 'view_on_twitter')
+    actions = None
+
+    readonly_fields = ('ref',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def view_on_twitter(self, obj):
+        url = 'https://twitter.com/i/status/{}'.format(obj.ref)
+        return mark_safe(
+            '<a href="{}" target="_blank">View on Twitter</a>'.format(url)
+        )
+    view_on_twitter.allow_tags = True
+    view_on_twitter.short_description = 'View on Twitter'
