77ac5ebcbcee79695dc752dc8dcf61b861601365
Hector Dominguez <hedb_12@hotmail.com>
2018-03-30 22:17:36 -0400

    only use better models

--- flu_tracker/classifiers/management/commands/train_model.py
+++ flu_tracker/classifiers/management/commands/train_model.py
@@ -18,97 +18,49 @@ CLASS_NAMES = {
 
 
 MODEL_CLASSES = {
-    'awareness-infection': [MultinomialNB, SGDClassifier],
-    'related-notrelated': [MultinomialNB, SGDClassifier],
-    'self-others': [MultinomialNB, SGDClassifier],
+    'awareness-infection': MultinomialNB,
+    'related-notrelated': MultinomialNB,
+    'self-others': SGDClassifier,
 }
 
 MODEL_PARAMETERS = {
-    'awareness-infection': [
-        {                       # MultinomialNB
-            'vectorizer': {
-                'ngram_range': (1, 3)
-            },
-            'term_frequency': {
-                'sublinear_tf': True,
-                'use_idf': False
-            },
-            'classifier': {
-                'alpha': 0.1
-            }
+    'awareness-infection': {    # MultinomialNB
+        'vectorizer': {
+            'ngram_range': (1, 3)
         },
-        {                       # SGDClassifier
-            'vectorizer': {
-                'ngram_range': (1, 3)
-            },
-            'term_frequency': {
-                'smooth_idf': False,
-                'norm': 'l1'
-            },
-            'classifier': {
-                'alpha': 1e-05,
-                'penalty': 'l1',
-                'warm_start': True,
-                'fit_intercept': False
-            }
-        }
-    ],
-    'related-notrelated': [
-        {                       # MultinomialNB
-            'vectorizer': {},
-            'term_frequency': {
-                'sublinear_tf': True,
-                'use_idf': False,
-                'norm': 'l1',
-            },
-            'classifier': {
-                'alpha': 0.1,
-                'fit_prior': False,
-            }
+        'term_frequency': {
+            'sublinear_tf': True,
+            'use_idf': False
         },
-        {                       # SGDClassifier
-            'vectorizer': {
-                'ngram_range': (1, 3)
-            },
-            'term_frequency': {},
-            'classifier': {
-                'alpha': 0.0001,
-                'penalty': 'l1',
-                'warm_start': True,
-                'shuffle': False,
-                'loss': 'log',
-            }
+        'classifier': {
+            'alpha': 0.1
         }
-    ],
-    'self-others': [
-        {                       # MultinomialNB
-            'vectorizer': {
-                'ngram_range': (1, 2)
-            },
-            'term_frequency': {
-                'sublinear_tf': True,
-                'use_idf': False,
-                'norm': 'l1',
-            },
-            'classifier': {
-                'alpha': 0.01,
-                'fit_prior': False,
-            }
+    },
+    'related-notrelated': {     # MultinomialNB
+        'vectorizer': {},
+        'term_frequency': {
+            'sublinear_tf': True,
+            'use_idf': False,
+            'norm': 'l1',
         },
-        {                       # SGDClassifier
-            'vectorizer': {
-                'ngram_range': (1, 3)
-            },
-            'term_frequency': {
-                'norm': 'l1'
-            },
-            'classifier': {
-                'alpha': 1e-05,
-                'penalty': 'none',
-                'warm_start': True
-            }
+        'classifier': {
+            'alpha': 0.1,
+            'fit_prior': False,
         }
-    ],
+    },
+    'self-others': {            # SGDClassifier
+        'vectorizer': {
+            'ngram_range': (1, 3)
+        },
+        'term_frequency': {
+            'norm': 'l1'
+        },
+        'classifier': {
+            'alpha': 1e-05,
+            'penalty': 'none',
+            'warm_start': True
+        }
+    },
 }
 
 
@@ -135,23 +87,22 @@ class Command(BaseCommand):
             df_train = read_csv(str(settings.DATASET_DIR.path(model).path('train.csv')))
             df_test = read_csv(str(settings.DATASET_DIR.path(model).path('test.csv')))
 
-            for i, cls_model in enumerate(MODEL_CLASSES[model]):
-                classifier_pipeline = Pipeline([             # text --> matrix of token counts
-                    ('vectorizer', CountVectorizer(
-                        **MODEL_PARAMETERS[model][i]['vectorizer']
-                    )),
-                    ('term_frequency', TfidfTransformer(     # count matrix -->  normalized TF or TF-IDF
-                        **MODEL_PARAMETERS[model][i]['term_frequency']
-                    )),
-                    ('classifier', cls_model(                # Naive Bayes (NB) or Support Vector Machine
-                        **MODEL_PARAMETERS[model][i]['classifier']
-                    ))
-                ])
+            classifier_pipeline = Pipeline([             # text --> matrix of token counts
+                ('vectorizer', CountVectorizer(
+                    **MODEL_PARAMETERS[model]['vectorizer']
+                )),
+                ('term_frequency', TfidfTransformer(     # count matrix -->  normalized TF or TF-IDF
+                    **MODEL_PARAMETERS[model]['term_frequency']
+                )),
+                ('classifier', MODEL_CLASSES[model](     # Naive Bayes (NB) or Support Vector Machine
+                    **MODEL_PARAMETERS[model]['classifier']
+                ))
+            ])
 
-                classifier = classifier_pipeline.fit(df_train.text, df_train.target)
-                predicted = classifier.predict(df_test.text)
-                accuracy = mean(predicted == df_test.target)
+            classifier = classifier_pipeline.fit(df_train.text, df_train.target)
+            predicted = classifier.predict(df_test.text)
+            accuracy = mean(predicted == df_test.target)
 
-                self.stdout.write('* {}: {}'.format(CLASS_NAMES[cls_model], accuracy))
+            self.stdout.write('* {}: {}'.format(CLASS_NAMES[MODEL_CLASSES[model]], accuracy))
 
-            self.stdout.write(self.style.SUCCESS('Classifiers trained'))
+        self.stdout.write(self.style.SUCCESS('Classifiers trained'))
