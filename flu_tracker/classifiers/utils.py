f62465f1edcb31767e82b1937f7bf125aa38d0ed
Hector Dominguez <hedb_12@hotmail.com>
2018-04-02 10:53:12 -0400

    create method to plot learning curves

--- flu_tracker/classifiers/utils.py
+++ flu_tracker/classifiers/utils.py
@@ -1,6 +1,12 @@
-from pickle import load
+from os import makedirs
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 from django.conf import settings
+
+from pickle import load
+from sklearn.model_selection import learning_curve
 
 
 def load_classifiers():
@@ -15,3 +21,56 @@ def load_classifiers():
             classifiers[model] = load(f)
 
     return classifiers
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Mostly taken from:
+    https://github.com/scikit-learn/scikit-learn/blob/0.19.1/examples/model_selection/plot_learning_curve.py
+    """
+
+    fig = plt.figure()
+    plt.title(title)
+
+    if ylim is not None:
+        plt.ylim(*ylim)
+
+    plt.xlabel('Training examples')
+    plt.ylabel('Score')
+
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes
+    )
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color='r')
+
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1,
+                     color='g')
+
+    plt.plot(train_sizes, train_scores_mean, 'o-', color='r',
+             label='Training score')
+
+    plt.plot(train_sizes, test_scores_mean, 'o-', color='g',
+             label='Cross-validation score')
+
+    plt.legend(loc='best')
+
+    # save figure
+    directory = str(settings.MODEL_DIR.path('learning_curves'))
+    makedirs(directory, exist_ok=True)
+
+    fig.savefig('{}/{}'.format(directory, title))
+    plt.close(fig)
