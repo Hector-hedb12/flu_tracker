77e6f5772be63891e2780ec8157fd3dfa487ce91
Hector Dominguez <hedb_12@hotmail.com>
2018-04-02 14:19:28 -0400

    create methods to plot precision-recall curves and
    ROC curves

--- flu_tracker/classifiers/utils.py
+++ flu_tracker/classifiers/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 from django.conf import settings
 
 from pickle import load
+from sklearn.metrics import average_precision_score, precision_recall_curve, roc_curve
 from sklearn.model_selection import learning_curve, validation_curve
 
 
@@ -110,6 +111,72 @@ def plot_validation_curve(estimator, title, X, y, param_name, param_range,
 
     # save figure
     directory = str(settings.MODEL_DIR.path('validation_curves'))
+    makedirs(directory, exist_ok=True)
+
+    fig.savefig('{}/{}'.format(directory, title))
+    plt.close(fig)
+
+
+def plot_precision_recall_curve(estimator, title, X, y):
+    """
+    Generate a simple precision recall plot.
+    """
+
+    fig = plt.figure()
+
+    if hasattr(estimator, 'decision_function'):
+        y_score = estimator.decision_function(X)
+    else:
+        y_score = estimator.predict_proba(X)[:, 0]
+
+    average_precision = average_precision_score(y, y_score)
+    precision, recall, _ = precision_recall_curve(y, y_score)
+
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+
+    plt.title('{0}: average={1:0.2f}'.format(title, average_precision))
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.05])
+
+    # save figure
+    directory = str(settings.MODEL_DIR.path('precision_recall_curves'))
+    makedirs(directory, exist_ok=True)
+
+    fig.savefig('{}/{}'.format(directory, title))
+    plt.close(fig)
+
+
+def plot_roc_curves(estimators, title, X, y):
+    """
+    Generate a ROC curves.
+    """
+
+    fig = plt.figure()
+
+    plt.plot([0, 1], [0, 1], 'k--')
+
+    for estimator, name in estimators:
+        if hasattr(estimator, 'decision_function'):
+            y_score = estimator.decision_function(X)
+        else:
+            y_score = estimator.predict_proba(X)[:, 1]
+
+        fpr, tpr, _ = roc_curve(y, y_score)
+
+        plt.plot(fpr, tpr, label=name)
+
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+
+    # save figure
+    directory = str(settings.MODEL_DIR.path('roc_curves'))
     makedirs(directory, exist_ok=True)
 
     fig.savefig('{}/{}'.format(directory, title))
